13a5c4da5d7db06a624727ceed2f75ccef97c1bc
sarodriguez28 <sarodriguez28@miners.utep.edu>
2025-03-02 21:46:59 -0700

    Code5attempt2

--- code_4.py
+++ code_4.py
@@ -1,9 +1,10 @@
 import csv
 import pandas as pd
 big_mac_file = './big-mac-full-index.csv'
+df = pd.read_csv(big_mac_file)
 
 def get_big_mac_price_by_year(year,country_code):
-    filtered = df[(df['date'].str.startwith(str(year))) & (df['iso_a3'].str.lower() == country_code.lower())]
+    filtered = df[(df['date'].str.startswith(str(year))) & (df['iso_a3'].str.lower() == country_code.lower())]
     return round(filtered['dollar_price'].mean(), 2)
 
 def get_big_mac_price_by_country(country_code):
@@ -11,7 +12,7 @@ def get_big_mac_price_by_country(country_code):
     return round(filtered['dollar_price'].mean(), 2)
 
 def get_the_cheapest_big_mac_price_by_year(year):
-    filtered = df[(df['date'].str.startwith(str(year)))]
+    filtered = df[(df['date'].str.startswith(str(year)))]
     cheapest = filtered.loc[filtered['dollar_price'].idxmin()]
     return f"{cheapest['name']}({cheapest['iso_a3']}): ${round(cheapest['dollar_price'], 2)}"
 
@@ -26,5 +27,5 @@ if __name__ == "__main__":
 
     print(f"\nAverage Big Mac price in {year} for {country_code.upper()}: ${get_big_mac_price_by_year(year, country_code)}")
     print(f"Average Big Mac price in {country_code.upper()}: ${get_big_mac_price_by_year(year, country_code)}")
-    print(f"Cheapest Big Mac price in {year}: {get_the_most_expensive_big_mac_price_by_year}")
-    print(f"Most expensive Bif Mac price in {year}: {get_the_most_expensive_big_mac_price_by_year(year)}")
+    print(f"Cheapest Big Mac price in {year}: {get_the_most_expensive_big_mac_price_by_year(year)}")
+    print(f"Most expensive Big Mac price in {year}: {get_the_most_expensive_big_mac_price_by_year(year)}")
